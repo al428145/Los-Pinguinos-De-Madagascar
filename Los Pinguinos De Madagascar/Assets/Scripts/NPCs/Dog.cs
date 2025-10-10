d7ef9381a395111a2e64f8be9e6649bf19a1aa6e
CrespoMejiasLuis <113623147+CrespoMejiasLuis@users.noreply.github.com>
2025-10-10 19:50:48 +0200

    Perro corregido((ya no se para)

--- Los Pinguinos De Madagascar/Assets/Scripts/NPCs/Dog.cs
+++ Los Pinguinos De Madagascar/Assets/Scripts/NPCs/Dog.cs
@@ -27,20 +27,31 @@ public class Dog : NPCBase
         FSM.Update();
         PlayerStillInRange = false;
         PlayerIsBeingSeen = false;
+
+        if (CurrentDestination != Vector3.zero)
+        {
+            Debug.DrawLine(transform.position, CurrentDestination, Color.red);
+        }
     }
 
     public override void SelectNewDestination()
     {
         if (zonaPatrulla == null) return;
 
+        float margen = 1.0f;
+
         Vector3 centro = zonaPatrulla.transform.TransformPoint(zonaPatrulla.center);
         Vector3 tamanio = Vector3.Scale(zonaPatrulla.size, zonaPatrulla.transform.lossyScale) / 2f;
 
-        float x = Random.Range(-tamanio.x, tamanio.x);
-        float z = Random.Range(-tamanio.z, tamanio.z);
+       
+        float rangoX = Mathf.Max(0, tamanio.x - margen);
+        float rangoZ = Mathf.Max(0, tamanio.z - margen);
+
+        float x = Random.Range(-rangoX, rangoX);
+        float z = Random.Range(-rangoZ, rangoZ);
         Vector3 punto = centro + new Vector3(x, 0f, z);
 
         CurrentDestination = new Vector3(punto.x, transform.position.y, punto.z);
-        Debug.Log(CurrentDestination);
+        Debug.Log("Nuevo destino con margen: " + CurrentDestination);
     }
 }
