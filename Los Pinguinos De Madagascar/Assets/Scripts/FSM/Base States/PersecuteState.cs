32f04383d00244fa96d777ceb75869e48ff75dfd
martamz33 <al427731@uji.es>
2025-10-10 17:48:01 +0200

    puertas

--- Los Pinguinos De Madagascar/Assets/Scripts/FSM/Base States/PersecuteState.cs
+++ Los Pinguinos De Madagascar/Assets/Scripts/FSM/Base States/PersecuteState.cs
@@ -5,22 +5,18 @@ public class PersecuteState : State
 {
     public override void Enter(NPCBase owner)
     {
-        Debug.Log($"{owner.name} comienza la persecución!");
+        Debug.Log($"{owner.name} comienza la persecucion!");
     }
 
     public override void Execute(NPCBase owner)
     {
-        // Aquí podrías usar tu Pathfinder:
-        if (owner is Dog dog)
-        {
-            WaypointManager wm = Object.FindObjectOfType<WaypointManager>();
-            if (wm == null) return;
+        WaypointManager wm = Object.FindObjectOfType<WaypointManager>();
+        if (wm == null) return;
 
-            List<Waypoint> waypoints = wm.GetWaypoints();
-            Waypoint enemyWaypoint = Pathfinder.FindTheNearestWaypointEnemy(dog.transform.position, dog.player.transform.position, waypoints);
-            Waypoint playerWaypoint = Pathfinder.FindNearestWaypointPlayer(dog.player.transform.position, waypoints);
-            List<Waypoint> ruta = Pathfinder.FindPath(enemyWaypoint, playerWaypoint);
-        }
+        List<Waypoint> waypoints = wm.GetWaypoints();
+        Waypoint enemyWaypoint = Pathfinder.FindTheNearestWaypointEnemy(owner.transform.position, owner.player.transform.position, waypoints);
+        Waypoint playerWaypoint = Pathfinder.FindNearestWaypointPlayer(owner.player.transform.position, waypoints);
+        List<Waypoint> ruta = Pathfinder.FindPath(enemyWaypoint, playerWaypoint);
     }
 
     public override System.Type GetNextStateForEvent(StateEvent evt)
